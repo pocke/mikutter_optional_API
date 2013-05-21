786b704a6e2488720621ba8bd167d485d341e6b6
pocke <p.ck.t22@gmail.com>
2013-05-21 14:00:03 +0900

    インデントの修正

--- optional_API.rb
+++ optional_API.rb
@@ -5,9 +5,9 @@ Plugin.create(:optional_API) do
 
   command(:optional_API,
           name: '任意のAPIを実行',
-	  condition: lambda{ |opt| true },
-	  visible: true,
-	  role: :postbox) do |opt|
+          condition: lambda{ |opt| true },
+          visible: true,
+          role: :postbox) do |opt|
     val = Plugin.create(:gtk).widgetof(opt.widget).widget_post.buffer.text
     val = JSON.parse(val, {:symbolize_names => true})
     endpoint = val.delete(:API)
